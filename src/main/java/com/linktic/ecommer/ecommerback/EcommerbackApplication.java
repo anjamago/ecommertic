125e0a0879a6193c77e0e262f3f2f1addd1583ae
AndyJavierMartinezGo <ajmartinez@qvision.com.co>
2024-06-07 18:16:20 -0500

    inicializacion de proyecto

---
+++ src/main/java/com/linktic/ecommer/ecommerback/EcommerbackApplication.java
@@ -0,0 +1,13 @@
+package com.linktic.ecommer.ecommerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linktic/ecommer/ecommerback/EcommerbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linktic.ecommer.ecommerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
