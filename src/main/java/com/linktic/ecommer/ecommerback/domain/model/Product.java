d297d97e9798af834e463233a36e845d8a373ffe
andy martinez <anjamago@gmail.com>
2024-06-08 15:20:08 -0500

    feat: Compleando flujos

--- src/main/java/com/linktic/ecommer/ecommerback/domain/model/Product.java
+++ src/main/java/com/linktic/ecommer/ecommerback/domain/model/Product.java
@@ -1,10 +1,6 @@
 package com.linktic.ecommer.ecommerback.domain.model;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
