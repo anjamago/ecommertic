5c1d79aafa134de36d6fab482d5bf0749da5a7e8
AndyJavierMartinezGo <ajmartinez@qvision.com.co>
2024-06-12 10:08:47 -0500

    feat: adicion de documentador swagger

--- src/main/java/com/linktic/ecommer/ecommerback/infrastructure/Api/ProductController.java
+++ src/main/java/com/linktic/ecommer/ecommerback/infrastructure/Api/ProductController.java
@@ -1,10 +1,13 @@
 package com.linktic.ecommer.ecommerback.infrastructure.Api;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.linktic.ecommer.ecommerback.application.ProductService;
 import com.linktic.ecommer.ecommerback.domain.model.ProductRequestDto;
 import com.linktic.ecommer.ecommerback.domain.model.ProductResponseDto;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
@@ -24,6 +27,14 @@ public class ProductController {
         return _productService.addProduct(productRequestDto);
     }
 
+    @PostMapping("upload")
+    public String UploadProduct(
+            @RequestParam("image") MultipartFile file,
+            @RequestParam("data") String data) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ProductRequestDto productResponseDto = objectMapper.readValue(data,ProductRequestDto.class);
+        return "Ok";
+    }
     @GetMapping()
     public List<ProductResponseDto> GetAllProducts() {
         return _productService.getAllProducts();
